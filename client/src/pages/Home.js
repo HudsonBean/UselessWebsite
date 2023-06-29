45ecd9b489cf50ea5332b6440bf699c58bcf3ece
Hudson <hudsonbeandev@gmail.com>
2023-06-29 13:11:23 -0500

    Done testing with Api's

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,39 +4,9 @@ import { json } from "react-router-dom";
 
 //Main
 function Home() {
-  const [textBox, setTextBox] = useState("");
-  const test = useRef();
-
-  function submitForm(e) {
-    e.preventDefault();
-    fetch("/api", {
-      method: "POST",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify({ message: textBox }),
-    });
-  }
-
-  function getData() {
-    fetch("/api", { method: "GET" })
-      .then((Response) => Response.json())
-      .then((data) => {
-        setTextBox(data.message);
-        test.current.value = data.message;
-      });
-  }
-
   return (
     <div>
       <h1>Welcome to my website!</h1>
-      <form onSubmit={submitForm}>
-        <input
-          type="text"
-          ref={test}
-          onChange={(e) => setTextBox(e.target.value)}
-        ></input>
-        <input type="submit"></input>
-        <button onClick={getData}>Get</button>
-      </form>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -7,17 +7,8 @@ app.use(express.json());
 //Variables
 
 //Gets
-app.get("/api", (req, res) => {
-  res.json({ message: "No message saved yet!" });
-});
 
 //Posts
-app.post("/api", (req, res) => {
-  if (!req.body) {
-    res.status(400).send("Error whilst receiving your message.");
-  }
-  res.status(200).send("Succesfully received your message!");
-});
 
 //Port start
 app.listen(8000, (err) => {
