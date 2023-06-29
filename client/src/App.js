14188d421a17651ab56cab131fd5ac27274aa7d1
Hudson <hudsonbeandev@gmail.com>
2023-06-28 20:17:51 -0500

    Set up multipage react and created a homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 //Imports
-import React, { useState, useEffect } from "react";
-import "./styles/App.css";
+import React from "react";
+import { Route, Link, Routes } from "react-router-dom";
+//Pages
+import Home from "./pages/Home.js";
 
 //Main
 function App() {
-  return <>Hello World!</>;
+  return (
+    <div>
+      <Routes>
+        <Route exact path="/" Component={Home} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,9 @@
+//Imports
+import React from "react";
+
+//Main
+function Home() {
+  return <div>Welcome to my website!</div>;
+}
+
+export default Home;
