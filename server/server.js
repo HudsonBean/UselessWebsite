3ae2b7990ef0b2a3b4dabd7c11ed53868f6585c6
Hudson <hudsonbeandev@gmail.com>
2023-06-30 09:18:26 -0500

    test

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 const express = require("express");
-
+const mysql = require("mysql");
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "Copsreloaded62$",
+  database: "useless_website_db",
+});
 const app = express();
 
 app.use(express.json());
@@ -7,6 +13,11 @@ app.use(express.json());
 //Variables
 
 //Gets
+app.get("/api/db", (req, res) => {
+  const a = db.query("select * from users;");
+  console.log(a);
+  res.send(a);
+});
 
 //Posts
 
