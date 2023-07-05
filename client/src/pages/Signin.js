a0866d9d3e27ab0943cf7f5d4452749088b3aee8
Hudson <hudsonbeandev@gmail.com>
2023-07-05 13:41:04 -0500

    Changed font

--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -4,7 +4,13 @@ import React from "react";
 import "../styles/Signin.css";
 
 function Signin() {
-  return <div className="signin-body"></div>;
+  return (
+    <div className="signin-body">
+      <form>
+        <h2>Sign-in</h2>
+      </form>
+    </div>
+  );
 }
 
 export default Signin;
